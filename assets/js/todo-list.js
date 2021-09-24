481046b1a0cda152e5da02af281cbfec2ed68b1c
Chris Nelson <chris@gaslight.co>
2021-09-23 21:07:42 -0400

    add todo event dispatched

--- assets/js/todo-list.js
+++ assets/js/todo-list.js
@@ -7,11 +7,20 @@ class TodoList extends LitElement {
     }
   }
 
+  get itemInput() { return this.shadowRoot.querySelector('input[name="todo_item"]'); }
+
+  addTodo(_event) {
+    this.dispatchEvent(new CustomEvent('add_todo', {detail: {item: this.itemInput.value}}));
+    this.itemInput.value = '';
+  }
+
   render() {
     return html`
       <ul>
         ${this.items.map(item => html`<li>${item}</li>`)}
       </ul>
+      <input name="todo_item" />
+      <button @click=${this.addTodo}>Add Item</button>
     `
   }
 }
