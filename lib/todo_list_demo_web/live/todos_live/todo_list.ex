def14ae926e9b88d65cbde4123950d264f9b0f1e
Chris Nelson <chris@gaslight.co>
2021-09-23 21:20:52 -0400

    handle the event in liveview

--- lib/todo_list_demo_web/live/todos_live/todo_list.ex
+++ lib/todo_list_demo_web/live/todos_live/todo_list.ex
@@ -5,4 +5,7 @@ defmodule TodoListDemoWeb.TodosLive.TodoList do
     {:ok, socket |> assign(todo_items: ["Put your left foot in", "Take your left foot out"])}
   end
 
+  def handle_event("add_todo", %{"item" => item}, %{assigns: %{todo_items: todo_items}} = socket) do
+    {:noreply, socket |> assign(todo_items: todo_items ++ [item])}
+  end
 end
