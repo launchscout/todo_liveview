e4ceaa41f64dc81ccc6faec94a66d70ee2a946dd
Chris Nelson <chris@gaslight.co>
2021-09-25 19:12:31 -0400

    due dates for todo items

--- assets/js/todo-list.js
+++ assets/js/todo-list.js
@@ -8,18 +8,26 @@ class TodoList extends LitElement {
   }
 
   get itemInput() { return this.shadowRoot.querySelector('input[name="todo_item"]'); }
+  get dueDateInput() { return this.shadowRoot.querySelector('input[name="due_date"]'); }
 
   addTodo(_event) {
-    this.dispatchEvent(new CustomEvent('add_todo', {detail: {item: this.itemInput.value}}));
+    this.dispatchEvent(new CustomEvent('add_todo', {
+      detail: {
+        item: this.itemInput.value, 
+        due_date: this.dueDateInput.value
+      }
+    }));
     this.itemInput.value = '';
+    this.dueDateInput.value = '';
   }
 
   render() {
     return html`
       <ul>
-        ${this.items.map(item => html`<li>${item}</li>`)}
+        ${this.items.map(item => html`<li>${item.item}</li>`)}
       </ul>
       <input name="todo_item" />
+      <input type="date" name="due_date" />
       <button @click=${this.addTodo}>Add Item</button>
     `
   }
